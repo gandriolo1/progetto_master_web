c125be66e620f7a988b06b789f20ba3613283c9f
gandriolo1 <giuliaa2009@hotmail.it>
2025-02-17 10:40:39 +0100

    primo commit-DB

---
+++ DB/SCRIPT/2_INSERT.sql
@@ -0,0 +1,20 @@
+INSERT INTO PRODOTTI (NOME, DESCRIZIONE, PREZZO, IMMAGINE, CATEGORIA, COLORE, MATERIALE, DATA_INSERIMENTO)
+VALUES ('Maglioncino Beige', 'Maglioncino in lana lavorato a mano con dettagli traforati e un bottone centrale.', 44.99, 'Maglioncino_Beige.png', 'Abbigliamento', 'Beige', 'Lana', '28-12-2024');
+
+INSERT INTO PRODOTTI (NOME, DESCRIZIONE, PREZZO, IMMAGINE, CATEGORIA, COLORE, MATERIALE, DATA_INSERIMENTO)
+VALUES ('Guantini Verde Salvia', 'Guantini in lana lavorati a mano. Rifiniti con un fiocchetto decorativo.', 23.49, 'Guantini1.png', 'Accessori', 'Verde', 'Lana', '27-12-2024');
+
+INSERT INTO PRODOTTI (NOME, DESCRIZIONE, PREZZO, IMMAGINE, CATEGORIA, COLORE, MATERIALE, DATA_INSERIMENTO)
+VALUES ('Scarpine Giallo Sole', 'Scarpine in lana lavorate a mano, comode e calde. Design elegante.', 29.99, 'Scarpine1.png', 'Calzature', 'Giallo', 'Lana', '27-12-2024');
+
+INSERT INTO PRODOTTI (NOME, DESCRIZIONE, PREZZO, IMMAGINE, CATEGORIA, COLORE, MATERIALE, DATA_INSERIMENTO)
+VALUES ('Bavaglino Fantasia a Fiori', '', 19.99, 'Bavaglino1.png', 'Accessori', 'Fantasia', 'Cotone', '31-12-2024');
+
+INSERT INTO PRODOTTI (NOME, DESCRIZIONE, PREZZO, IMMAGINE, CATEGORIA, COLORE, MATERIALE, DATA_INSERIMENTO)
+VALUES ('Bavaglino Fantasia con Decori', '', 19.99, 'Bavaglino2.png', 'Accessori', 'Fantasia', 'Cotone', '31-12-2024');
+
+INSERT INTO PRODOTTI (NOME, DESCRIZIONE, PREZZO, IMMAGINE, CATEGORIA, COLORE, MATERIALE, DATA_INSERIMENTO)
+VALUES ('Cappellino con Pon Pon', '', 39.99, 'Cappellino1.png', 'Accessori', 'Arancione', 'Lana', '31-12-2024');
+
+INSERT INTO PRODOTTI (NOME, DESCRIZIONE, PREZZO, IMMAGINE, CATEGORIA, COLORE, MATERIALE, DATA_INSERIMENTO)
+VALUES ('Gonnellina', '', 50.50, 'Gonnellina1.png', 'Abbigliamento', 'Rosso', 'Ciniglia', '06-01-2025');
